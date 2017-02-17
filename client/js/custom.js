668bc24833ef009fa814acfcbd72658b4c5aaac1
YOMA <email": null>
2017-02-17 11:17:31 +0700

    tambahan deskripsi dan perbaikan halaman login

--- client/js/custom.js
+++ client/js/custom.js
@@ -10,14 +10,12 @@ function ready () {
     type: 'GET',
     url: 'http://localhost:3000/list',
     success: function (data) {
-      //   console.log(data)
-      // console.log(JSON.parse(data))
       let booklist = data
 
       for (var i = 0; i < booklist.length; i++) {
         let item = booklist[i]
         let table = `<div class="box-item">
-            <h2>Item Found</h2>
+            <h2>${item.namabarang}</h2>
             <div class="row">
               <div class="col-3">
                 <img src="http://localhost:3000/images/${item.photobarang}" alt="">
@@ -25,6 +23,7 @@ function ready () {
               <div class="col-9">
                 <form>
                   <h3>Description : </h3>
+                  <p>${item.description}</p>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                   <h4 class="notfound" >Not Found</h4>
                 </form>
